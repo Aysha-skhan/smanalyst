31ee4ffbd64d156627cedb42c97469a8a58e2063
Aysha-skhan <ayeshasakkhan@gmail.com>
2026-03-20 02:34:27 +0500

    feat: dataset preview endpoint

--- datasets/urls.py
+++ datasets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import upload_dataset,list_datasets
+from .views import upload_dataset,list_datasets,preview_datasets
 
 urlpatterns=[
     path('upload/', upload_dataset),
     path('list/', list_datasets),
+    path('<int:dataset_id>/preview/',preview_datasets)
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
+import pandas as pd
 from .serializers import DatasetSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Dataset
+
 
 @api_view(['POST'])
 def upload_dataset(request):
@@ -17,6 +19,23 @@ def list_datasets(request):
     datasets = Dataset.objects.filter(uploaded_by=request.user)
     serializer=DatasetSerializer(datasets, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def preview_datasets(request, dataset_id):
+    dataset = Dataset.objects.get(id=dataset_id)
+    if dataset.uploaded_by != request.user:
+        return Response({'error': 'Not authorized'}, status=status.HTTP_403_FORBIDDEN)
+    df = pd.read_csv(dataset.file.path)
+    data=df.head(10).to_dict(orient='records')
+    column_names=list(df.columns)
+    return Response({
+    'columns': column_names,
+    'rows': data
+})
+    
+        
+
+
 
 
 
