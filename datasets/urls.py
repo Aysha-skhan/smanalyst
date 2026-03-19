e7023cc160f7181a5f24592f1fcd4178b9bef4ee
Aysha-skhan <ayeshasakkhan@gmail.com>
2026-03-20 01:58:08 +0500

    Added list datasets endpoint

--- datasets/urls.py
+++ datasets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import upload_dataset
+from .views import upload_dataset,list_datasets
 
 urlpatterns=[
-    path('upload/', upload_dataset)
+    path('upload/', upload_dataset),
+    path('list/', list_datasets),
 ]
--- datasets/views.py
+++ datasets/views.py
@@ -3,6 +3,7 @@ from .serializers import DatasetSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Dataset
 
 @api_view(['POST'])
 def upload_dataset(request):
@@ -11,6 +12,12 @@ def upload_dataset(request):
         serializer.save(uploaded_by=request.user)
         return Response(serializer.data,status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET'])
+def list_datasets(request):
+    datasets = Dataset.objects.filter(uploaded_by=request.user)
+    serializer=DatasetSerializer(datasets, many=True)
+    return Response(serializer.data)
+
 
 
     
